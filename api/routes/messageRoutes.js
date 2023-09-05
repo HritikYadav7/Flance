d36a895300c06fcb15d3a5ea2f600950d6533ce7
Yogesh Kumar Yadav <97102768+HritikYadav7@users.noreply.github.com>
2023-09-05 18:28:29 +0530

    Message working

--- api/routes/messageRoutes.js
+++ api/routes/messageRoutes.js
@@ -7,4 +7,5 @@ const router = express.Router();
 router.post("/", verifyJWT.verifyToken, messageController.createMessage);
 router.get("/:id", verifyJWT.verifyToken, messageController.getMessages);
 
+
 export default router;
